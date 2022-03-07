71b6704b665cf451d47f0aacd34a67c538de4ac1
Radgus <dominguezradgus@gmail.com>
2022-03-06 22:58:18 -0600

    product section added

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,7 +4,8 @@ import { useFeaturedBanners } from '../../utils/hooks/useFeaturedBanners';
 import SliderSection from './components/SliderSection';
 import productsCategories from '../../utils/productsCategory';
 import CategorySection from './components/CategorySection';
-
+import ProductSection from './components/ProductSection';
+import productList from '../../utils/products';
 
 const SectionTitle = styled.div`
   margin-top: 3rem;
@@ -26,6 +27,8 @@ const Home = () => {
 
   const categoryList = productsCategories.results;
 
+  const productsList = productList.results
+
   return (
     <main>
       <SliderSection 
@@ -43,7 +46,7 @@ const Home = () => {
         <h1>Products</h1>
       </SectionTitle>
 
-      
+      <ProductSection productsList={productsList}/> 
 
     </main>
   )
