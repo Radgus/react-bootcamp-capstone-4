27ddba7aed353e2dc27c6a970d16ac0083688af5
Radgus <dominguezradgus@gmail.com>
2022-03-06 22:28:35 -0600

    Category section added

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,66 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useFeaturedBanners } from '../../utils/hooks/useFeaturedBanners';
+import SliderSection from './components/SliderSection';
+import productsCategories from '../../utils/productsCategory';
+import CategorySection from './components/CategorySection';
 
-const Slider = styled.div`
-  position: relative;
-  border-radius: 0.5rem;
-`;
 
-const SliderButton = styled.div`
-  width: 2rem;
-  height: 4rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: black;
-  position: absolute;
-  bottom: 50%;
-  right: ${(props) => props.isRight ? 0 : 'none'};
-  z-index: 10;
-  .arrow {
-    width: 1.5rem;
-    height: 1.5rem;
-    border-top: 0.2rem solid white;
-    border-left: 0.2rem solid white;
-    position: relative;
-  }
-  .left {
-    transform: rotate(-45deg);
-    left: 0.3rem;
-  }
-  .right {
-    transform: rotate(135deg);
-    right: 0.3rem;
-  }
-  @media (min-width: 320px) {
-    width: 3rem;
-    height: 6rem;
-  }
-`;
-
-const Displacer = styled.div`
-  width: 100%;
-  display: flex;
-  scroll-behavior: smooth;
-  overflow-x: scroll;
-  ::-webkit-scrollbar {
-  display: none;
-  }
-`;
-
-const Slide = styled.div`
-  min-width: 100%;
-`;
-
-const Img = styled.img`
-  width: 100%;
-  object-fit: contain;
+const SectionTitle = styled.div`
+  margin-top: 3rem;
 `;
 
 const Home = () => {
   const featuredBanners = useFeaturedBanners().data?.results;
-
   const handleSlider = (moveDirection) => {
     const contentSlider = document.getElementById('contentSlider');
     const childLength = contentSlider.firstChild.clientWidth;
@@ -73,27 +24,27 @@ const Home = () => {
     }
   }
 
+  const categoryList = productsCategories.results;
+
   return (
     <main>
-      <Slider>
-        <SliderButton onClick={() => handleSlider('left')}>
-          <div className="arrow left"/>
-        </SliderButton>
+      <SliderSection 
+        handleSlider={handleSlider} 
+        featuredBanners={featuredBanners}
+      />
 
-        <Displacer id='contentSlider'>
-          {
-            featuredBanners?.map((item) => 
-              <Slide key={item.id}>
-                <Img src={item.data.main_image.url} alt="" />
-              </Slide>
-            )
-          }
-        </Displacer>
+      <SectionTitle>
+        <h1>Categories</h1>
+      </SectionTitle>
 
-        <SliderButton isRight onClick={() => handleSlider('rigth')}>
-          <div className="arrow right"/>
-        </SliderButton>
-      </Slider>
+      <CategorySection categoryList={categoryList}/>
+      
+      <SectionTitle>
+        <h1>Products</h1>
+      </SectionTitle>
+
+      
+
     </main>
   )
 }
