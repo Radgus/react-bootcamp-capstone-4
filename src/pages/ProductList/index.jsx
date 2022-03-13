3bd0f9f3d2185c225d7087927d0e94f8bb81fa4e
Radgus <dominguezradgus@gmail.com>
2022-03-13 13:24:56 -0600

    sidebar and categories added

--- src/pages/ProductList/index.jsx
+++ src/pages/ProductList/index.jsx
@@ -4,13 +4,14 @@ import { Divider } from '../../components/Mix';
 import categoryIcon from '../../resources/images/category-icon2.png';
 import activeIcon from '../../resources/images/active-icon.png';
 import listIcon from '../../resources/images/list-icon.png';
+import CategoryList from '../../utils/CategoryList';
 
 
 const Container = styled.div`
   display: flex;
   margin-right: 8%;
   .container__sidebar {
-    width: ${(props) => props.collapsed ? '13%' : '35%'};
+    width: ${(props) => props.collapsed ? '4.25rem' : '36%'};
     background-color: white;
     overflow-y: hidden;
   }
@@ -39,17 +40,43 @@ const Sidebar = styled.section`
       height: 2.5rem;
     }
   }
+  .check {
+    height: 3rem;
+  }
+  .sidebar__bullet {
+    width: 0.6rem;
+    height: 0.6rem;
+    background-color: black;
+    border-radius: 50%;
+  }
 `;
 
 
 const ProductList = () => {
   const [isMenuCollapse,setIsMenuCollapse] = useState(false);
-  const [activeFilters, setActiveFilters] = useState({});
-  const [filterList, setFilterList] = useState({});
+  const [activeFilters, setActiveFilters] = useState([]);
+  const [categoryList, setCategoryList] = useState([]);
 
+  useEffect(() => {
+    setCategoryList([...CategoryList['results']]);
+  }, []);
 
-  const handleSidebar = () => {
-    setIsMenuCollapse(!isMenuCollapse);
+  useEffect(() => {
+    console.log('activeFilters: ', activeFilters);
+  }, [activeFilters]);
+
+  const handleSidebar = () => setIsMenuCollapse(!isMenuCollapse);
+  
+  const handlerCheckbox = (e,id) => {
+    const isActive = e.target.checked;
+    if (isActive) {
+      const filterSelected = categoryList.filter(item => item.id === id);
+      setActiveFilters(activeFilters.concat(filterSelected));
+    } else {
+      const newActiveFilters = activeFilters.filter(item => item.id !== id);
+      setActiveFilters(newActiveFilters);
+    }
+
   }
   
   return (
@@ -62,7 +89,7 @@ const ProductList = () => {
             <div className="sidebar__icon">
               <img src={categoryIcon} alt="filter Icon"/>
             </div>
-            <p>Category</p>
+            <p>Categories</p>
           </div>
           <Divider />
 
@@ -72,14 +99,45 @@ const ProductList = () => {
             </div>
             <p>List</p>
           </div>
+          { 
+            categoryList.map((category) => {
+              const id = category.id;
+              return (
+                <div key={id} className="sidebar__section">
+                  <div className="sidebar__icon check">
+                    <input 
+                      id={id} 
+                      type="checkbox" 
+                      onClick={(e) => handlerCheckbox(e,id)}
+                    />
+                  </div>
+                  <label htmlFor={id}>{category.data.name}</label>
+                </div>
+              );
+            })
+          }
           <Divider />
           
           <div className="sidebar__section">
             <div className="sidebar__icon">
               <img src={activeIcon} alt="filter Icon"/>
             </div>
-            <p>Active</p>
+            <p>Actived</p>
           </div>
+
+            { 
+              activeFilters.length > 0 && activeFilters.map((category) => {
+                const id = category.id;
+                return (
+                  <div key={id} className="sidebar__section">
+                    <div className="sidebar__icon check">
+                      <div className="sidebar__bullet"/>
+                    </div>
+                    <p>{category.data.name}</p>
+                  </div>
+                );
+              })
+            }
 
         </Sidebar>
       </div>
