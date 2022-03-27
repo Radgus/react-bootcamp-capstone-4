dbdc532a7d6b921a6c62b5b99a52f56650c9f208
Radgus <dominguezradgus@gmail.com>
2022-03-26 21:12:07 -0600

    specs added

--- src/pages/ProductDetail/index.jsx
+++ src/pages/ProductDetail/index.jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import { useFeaturedProducts } from "../../utils/hooks/useFeaturedProducts";
 import Gallery from "./Gallery";
 import {  useParams } from "react-router-dom";
-import { Divider } from "../../components/Mix";
 
 
 const Container = styled.div`
@@ -148,6 +147,93 @@ const Math = styled.div`
   }
 `;
 
+// Expand INICIO
+const ExpandContainer = styled.div`
+  padding: 1rem 2rem;
+  background-color: white;
+  .wrapper {
+    display: flex;
+    height: 3rem;
+  }
+  .wrapper__title {
+    width: 80%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .wrapper__icon {
+    display: flex;
+    align-items: center;
+    width: 20%;
+    cursor: pointer;
+  }
+  .content {
+    padding: 1rem 2rem 0;
+    display: grid;
+    grid-template-columns: repeat(auto-fill,minmax(45%, 1fr));
+    column-gap: 1rem;
+    row-gap: 1.5rem;
+    font-size: 1.1rem;
+    height: 0;
+    overflow: hidden;
+    transition: height 1s ease-in-out;
+    overflow-y: hidden;
+    scroll-behavior: smooth;
+  }
+`;
+
+const Arrow = styled.div`
+  width: 1.5rem;
+  height: 1.5rem;
+  border-right: 0.2rem solid black;
+  border-bottom: 0.2rem solid black;
+  transform: ${(props) => props.expand ? 'rotate(-135deg)' : 'rotate(45deg)'};
+  margin: 0 auto;
+`;
+
+const ExpandCard = ({product}) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  useEffect(()=>{
+    const node = document.getElementById('specContainer');
+    if (isExpanded) {
+      node.style.height = '20rem';
+    } else {
+      node.style.height = '0';
+    }
+  },[isExpanded]);
+
+  const handleExpand = () => {
+    setIsExpanded(!isExpanded);
+  }
+
+  return(
+    <ExpandContainer>
+      <div className="wrapper">
+        <h2 className="wrapper__title">
+          Specs
+        </h2>
+        <div className="wrapper__icon" onClick={handleExpand}>
+          <Arrow id="arrow" expand={isExpanded}/>
+        </div>
+      </div>
+      <div className="content" id="specContainer">
+        {
+          product?.data?.specs.map(spec=>(
+            <div key={spec.spec_name}>
+              <p><b>Name:</b> {spec.spec_name}</p>
+              <p><b>Value:</b> {spec.spec_value}</p>
+            </div>
+          ))
+        }
+      </div>
+    </ExpandContainer>
+  );
+}
+
+
+// Expand FIN
+
 
 
 const ProductDetail = () => {
@@ -188,7 +274,7 @@ const ProductDetail = () => {
             <h4>Tags:</h4>
             <Ul>
               {
-                product?.tags?.map(tag => <li>{tag}</li>)
+                product?.tags?.map(tag => <li key={tag}>{tag}</li>)
               }
             </Ul>
           </Tags>
@@ -232,7 +318,10 @@ const ProductDetail = () => {
             <Buttom type="button" onClick={(e)=>handleAddToCard(e, product)}>Add to Cart</Buttom>
           </div>
         </Shopping>
+
       </Card>
+
+      <ExpandCard product={product} />
       
     </Container>
   )
